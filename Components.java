47923e01759eed9b25e31e9779ec1d7ab06e88a5
Jonathan Sanchez <jcsanchez@Jonathans-MacBook-Pro-2.local>
2022-11-26 02:31:36 -0700

    Finish Robot and Components

---
+++ Components.java
@@ -0,0 +1,23 @@
+public class Components {
+    private String name;
+    private String functionalities;
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFunctionalities() {
+        return functionalities;
+
+    }
+
+    public void setFunctionalities(String functionalities) {
+        this.functionalities = functionalities;
+    }
+
+
+}
